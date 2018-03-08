bfe09ca40f585d89121027eedbfb5b24b9c7a568
matthewpbo <mpbolus@uab.edu>
2018-03-08 17:36:13 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+      res.sendFile( __dirname + "/" + "index.html" );
+})
+app.get('/process_get', function (req, res) {
+      response = {
+            first : req.query.first_name,
+            last : req.query.last_name
+      };
+      console.log(response);
+      res.end(JSON.stringify(response));
+})
+var server = app.listen(3000, function () {
+      var host = server.address().address
+      var port = server.address().port
+      console.log("Example app listening at http://%s:%s", host, port)
+})
+
+
